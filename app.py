f45151c90ba7356ceefef8ffcf00929cd47e544a
noa-ehrenhalt <73245647+noa-ehrenhalt@users.noreply.github.com>
2021-01-12 23:38:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from joblib import load
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
 app = Flask(__name__)
 with open('model.pkl', 'rb') as file:
